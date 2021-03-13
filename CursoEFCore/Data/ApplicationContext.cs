bbcaa695a68f6885de05d528021e527f507d3d74
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-13 12:13:18 -0300

    Exemplo de inserção de registro na base de dados

--- CursoEFCore/Data/ApplicationContext.cs
+++ CursoEFCore/Data/ApplicationContext.cs
@@ -5,6 +5,10 @@ namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data source=(localdb)\\MSSQLLocalDB;Initial Catalog=CursoEFCore;Integrated Security=true");
--- CursoEFCore/Program.cs
+++ CursoEFCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
 {
@@ -6,7 +9,32 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+
+            // Formas de rastrear uma entidade para posteriormente armazenar na base de dados
+            db.Produtos.Add(produto);
+            // db.Set<Produto>().Add(produto);
+            // db.Entry(produto).State = EntityState.Added;
+            // db.Add(produto); // adiciona uma pequena sobrecarga de processamento
+
+            // Salva as alterações na base de dados
+            var registros = db.SaveChanges(); // retorna a quantidade de registros afetados
+
+            Console.WriteLine($"Total Registro(s): {registros}");
         }
     }
 }
