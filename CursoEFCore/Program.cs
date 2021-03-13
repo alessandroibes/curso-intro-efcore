7daee06664f8d6ae94d67746478891314175ca03
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-13 15:48:32 -0300

    Exemplo de inserção de registros em massa na base de dados

--- CursoEFCore/Program.cs
+++ CursoEFCore/Program.cs
@@ -9,7 +9,67 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            InserirDados();
+            //InserirDados();
+
+            //InserirDadosEmMassa();
+
+            InserirDadosEmMassa2();
+        }
+
+        private static void InserirDadosEmMassa2()
+        {
+            var listaClientes = new[]
+            {
+                new Cliente()
+                {
+                    Nome = "Teste 1",
+                    CEP = "11111111",
+                    Cidade = "Cidade Teste 1",
+                    Estado = "SE",
+                    Telefone = "222222222"
+                },
+                new Cliente()
+                {
+                    Nome = "Teste 2",
+                    CEP = "33333333",
+                    Cidade = "Cidade Teste 2",
+                    Estado = "PI",
+                    Telefone = "4444444444"
+                }
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Clientes.AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto()
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente()
+            {
+                Nome = "Alessandro Oliveira",
+                CEP = "99999000",
+                Cidade = "Fortaleza",
+                Estado = "CE",
+                Telefone = "999990000"
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, cliente);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
         }
 
         private static void InserirDados()
